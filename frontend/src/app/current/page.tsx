cf09e18b23cf036ba3832882660d96f001363e2a
青木 真莉菜 <m.aoki.258@gmail.com>
2025-03-27 09:25:45 +0900

    update : transactionAPIの実装

--- frontend/src/app/current/page.tsx
+++ frontend/src/app/current/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AllTransactionPage from "./allTransaction/page";
+import AllTransaction from "./allTransaction/page";
 import MonthlyTransactionPage from "./monthlyTransaction/page";
 import TotalSavingPage from "./totalSaving/page";
 
@@ -8,11 +8,11 @@ const CurrentPage = () => {
     <section className="flex flex-row justify-center items-center w-full mt-10">
       <div className="flex flex-row w-full justify-between gap-12">
         <div className="text-center w-full border-r-2 border-dotted border-gray-400 p-4 min-h-[900px]">
-          <AllTransactionPage />
+          <AllTransaction />
         </div>
-        <div className="text-center w-full border-r-2 border-dotted border-gray-400 p-4 min-h-[900px]">
+        {/* <div className="text-center w-full border-r-2 border-dotted border-gray-400 p-4 min-h-[900px]">
           <MonthlyTransactionPage />
-        </div>
+        </div> */}
         <div className="text-center w-full p-4 min-h-[200px]">
           <TotalSavingPage />
         </div>
